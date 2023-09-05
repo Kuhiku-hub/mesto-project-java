e09c47cee2085b2940b6dd022e45a6d0299bb572
Kuhiku-hub <126280770+Kuhiku-hub@users.noreply.github.com>
2023-09-05 23:26:26 +0500

    Перепись PopupWithForm в ООП

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,53 @@
+import { Popup } from "./popup.js";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, { onFormSubmit }) {
+    super(popupSelector);
+    this._onFormSubmit = onFormSubmit;
+    this._popupForm = this._popupItem.querySelector(".popup__form");
+    this._inputList = Array.from(
+      this._popupForm.querySelectorAll(".popup__input")
+    );
+    this._sendButton = this._popupItem.querySelector(".popup__submit");
+    this._submitButtonText = this._sendButton.textContent;
+
+  }
+
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach((inputItem) => {
+      formValues[inputItem.name] = inputItem.value;
+    });
+    return formValues;
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    this._onFormSubmit(this._getInputValues());
+  }
+
+  addEventListeners() {
+    super.addEventListeners();
+    this._popupForm.addEventListener("submit", this._handleSubmit);
+  }
+
+  removeEventListeners() {
+    super.removeEventListeners();
+    this._popupForm.removeEventListener("submit", this._handleSubmit);
+  }
+
+  showSavingText() {
+    this._sendButton.textContent = "Сохранение...";
+  }
+
+  restoreSubmitButtonText() {
+    this._sendButton.textContent = this._submitButtonText;
+  }
+
+  closeAndReset() {
+    super.close();
+    this._popupForm.reset();
+  }
+}
+
+export { PopupWithForm };
